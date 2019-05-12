b8b40ce520a626e4bf518c3c0970dec616b6460f
Alenachka <lilovsky1988@gmail.com>
2019-05-12 12:52:02 +0200

    populate spinner, migrate to androidx

--- app/src/main/java/com/example/notekeeping/MainActivity.java
+++ app/src/main/java/com/example/notekeeping/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.notekeeping;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,14 +22,12 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById( R.id.toolbar );
         setSupportActionBar( toolbar );
 
-        FloatingActionButton fab = findViewById( R.id.fab );
-        fab.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make( view, "Replace with your own action", Snackbar.LENGTH_LONG )
-                        .setAction( "Action", null ).show();
-            }
-        } );
+        Spinner spinnerCourses = (Spinner) findViewById( R.id.spinner_courses );
+
+        List<CourseInfo> courses = DataManager.getInstance().getCourses();
+        ArrayAdapter<CourseInfo> adapterCourses = new ArrayAdapter<>( this, android.R.layout.simple_spinner_item, courses );
+        adapterCourses.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item );
+        spinnerCourses.setAdapter( adapterCourses );
     }
 
     @Override
