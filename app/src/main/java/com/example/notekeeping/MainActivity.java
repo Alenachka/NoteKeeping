98622d5b1a53131193b5d77ffcdcea8f5fcceb3a
Alenachka <lilovsky1988@gmail.com>
2019-05-12 17:08:02 +0200

    created new option menu - next, used icons for options

--- app/src/main/java/com/example/notekeeping/MainActivity.java
+++ app/src/main/java/com/example/notekeeping/MainActivity.java
@@ -123,9 +123,30 @@ public class MainActivity extends AppCompatActivity {
         } else if (id == R.id.action_cancel){
             mIsCancelling = true;
             finish();//activity will exit
+        } else if (id == R.id.action_next){
+            moveNext();
         }
 
         return super.onOptionsItemSelected( item );
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem item = menu.findItem( R.id.action_next );
+        int lastNoteIndex = DataManager.getInstance().getNotes().size()-1;
+        item.setEnabled( mNotePosition < lastNoteIndex );
+        return super.onPrepareOptionsMenu( menu );
+    }
+
+    private void moveNext() {
+        saveNote();
+
+        ++mNotePosition;
+        mNote = DataManager.getInstance().getNotes().get( mNotePosition );
+
+        saveOriginalNoteValues();
+        displayNote( mSpinnerCourses, mTextNoteTitle, mTextNoteText );
+        invalidateOptionsMenu(); //calls the method onPrepareOptionsMenu
     }
 
     @Override
