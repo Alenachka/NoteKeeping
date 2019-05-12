50b1b7f6808716c59f64039612c581c7d9a7463e
Alenachka <lilovsky1988@gmail.com>
2019-05-12 18:01:42 +0200

    added NavDrawer

---
+++ app/src/main/java/com/example/notekeeping/MenuActivity.java
@@ -0,0 +1,161 @@
+package com.example.notekeeping;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import android.view.View;
+
+import androidx.core.view.GravityCompat;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.Menu;
+
+import java.util.List;
+
+public class MenuActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private NoteRecyclerAdapter mNoteRecyclerAdapter;
+    private RecyclerView mRecyclerItems;
+    private LinearLayoutManager mNotesLayoutManager;
+    private CourseRecyclerAdapter mCourseRecyclerAdapter;
+    private GridLayoutManager mCoursesLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_menu );
+        Toolbar toolbar = findViewById( R.id.toolbar );
+        setSupportActionBar( toolbar );
+        FloatingActionButton fab = findViewById( R.id.fab );
+        fab.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity( new Intent(MenuActivity.this, MainActivity.class) );
+            }
+        } );
+
+        DrawerLayout drawer = findViewById( R.id.drawer_layout );
+        NavigationView navigationView = findViewById( R.id.nav_view );
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close );
+        drawer.addDrawerListener( toggle );
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener( this );
+        initializeDisplayContent();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //when we create new note we must say adapter that the data has changed
+        mNoteRecyclerAdapter.notifyDataSetChanged();
+    }
+
+    private void initializeDisplayContent() {
+        mRecyclerItems= (RecyclerView) findViewById( R.id.list_items );
+        mNotesLayoutManager = new LinearLayoutManager( this );
+        mCoursesLayoutManager = new GridLayoutManager( this, 2 );
+
+        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+        mNoteRecyclerAdapter = new NoteRecyclerAdapter( this, notes );
+
+        List<CourseInfo> courses = DataManager.getInstance().getCourses();
+        mCourseRecyclerAdapter = new CourseRecyclerAdapter( this, courses );
+
+        displayNotes();
+    }
+
+    private void displayCourses() {
+        mRecyclerItems.setLayoutManager( mCoursesLayoutManager );
+        mRecyclerItems.setAdapter( mCourseRecyclerAdapter );
+
+        selectNavMenuItem( R.id.nav_courses );
+    }
+
+    private void displayNotes() {
+        mRecyclerItems.setAdapter( mNoteRecyclerAdapter );
+        mRecyclerItems.setLayoutManager( mNotesLayoutManager );
+
+        selectNavMenuItem( R.id.nav_notes );
+    }
+
+    private void selectNavMenuItem(int id) {
+        NavigationView navigationView = (NavigationView) findViewById( R.id.nav_view );
+        Menu menu = navigationView.getMenu();
+        menu.findItem( id ).setChecked( true );
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById( R.id.drawer_layout );
+        if (drawer.isDrawerOpen( GravityCompat.START )) {
+            drawer.closeDrawer( GravityCompat.START );
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate( R.menu.menu, menu );
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected( item );
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_notes) {
+            displayNotes();
+        } else if (id == R.id.nav_courses) {
+            displayCourses();
+        } else if (id == R.id.nav_share) {
+            handleSelection("You have shared enough");
+        } else if (id == R.id.nav_send) {
+            handleSelection("Sende das irgendeiner sau!");
+        }
+
+        DrawerLayout drawer = findViewById( R.id.drawer_layout );
+        drawer.closeDrawer( GravityCompat.START );
+        return true;
+    }
+
+    private void handleSelection(String message) {
+        View view = findViewById( R.id.list_items );
+        Snackbar.make( view, message, Snackbar.LENGTH_LONG).show();
+    }
+}
--- app/src/main/java/com/example/notekeeping/NoteListActivity.java
+++ app/src/main/java/com/example/notekeeping/NoteListActivity.java
@@ -45,27 +45,10 @@ public class NoteListActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         //when we create new note we must say adapter that the data has changed
-//        mAdapterNotes.notifyDataSetChanged();
         mNoteRecyclerAdapter.notifyDataSetChanged();
     }
 
     private void initializeDisplayContent() {
-//        final ListView listNotes = (ListView) findViewById( R.id.list_notes );
-//
-//        List<NoteInfo> notes = DataManager.getInstance().getNotes();
-//        mAdapterNotes = new ArrayAdapter<>( this, android.R.layout.simple_list_item_1, notes );
-//
-//        listNotes.setAdapter( mAdapterNotes );
-//        listNotes.setOnItemClickListener( new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                Intent intent = new Intent( NoteListActivity.this, MainActivity.class );
-////                NoteInfo note = (NoteInfo) listNotes.getItemAtPosition( position );
-//                intent.putExtra( MainActivity.NOTE_POSITION, position );
-//                startActivity( intent );
-//            }
-//        } );
-
         final RecyclerView recyclerNotes = (RecyclerView) findViewById( R.id.list_notes );
         LinearLayoutManager notesLayoutManager = new LinearLayoutManager( this );
         recyclerNotes.setLayoutManager( notesLayoutManager );
