f00e28e674b304945a6d698e07d3a5f5a2034bdd
Alenachka <lilovsky1988@gmail.com>
2019-08-05 20:36:36 +0200

    implementing content provider

---
+++ app/src/main/java/com/example/notekeeping/NoteKeepingProviderContract.java
@@ -0,0 +1,35 @@
+package com.example.notekeeping;
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public final class NoteKeepingProviderContract {
+    private NoteKeepingProviderContract(){}
+    public static final String AUTHORITY = "com.example.notekeeping.provider";
+    public static final Uri AUTHORITY_URI = Uri.parse( "content://" + AUTHORITY );
+
+    protected interface CoursesIdColumns {
+        public static final String COLUMN_COURSE_ID = "course_id";
+    }
+
+    protected interface CoursesColumns{
+        public static final String COLUMN_COURSE_TITLE = "course_title";
+    }
+
+    protected interface NotesColumns{
+        public static final String COLUMN_NOTE_TEXT= "note_text";
+        public static final String COLUMN_NOTE_TITLE = "note_title";
+    }
+
+    public final static class Courses implements CoursesColumns, BaseColumns, CoursesIdColumns {
+        public static final String PATH = "courses";
+        //content://com.example.notekeeping.provider/courses
+        public static final Uri CONTENT_URI = Uri.withAppendedPath( AUTHORITY_URI, PATH );
+    }
+
+    public static final class Notes implements NotesColumns, BaseColumns, CoursesIdColumns {
+        public static final String PATH = "notes";
+        public static final Uri CONTENT_URI = Uri.withAppendedPath( AUTHORITY_URI,PATH );
+    }
+}
+
